405c434bc53b104daeba756216ed5f5d5bb356c5
Marsigli <marsigli1988@gmail.com>
2021-12-15 12:21:03 -0500

    built app.js, card.js, and cardsData.js rendering to screen

--- greetery/src/App.js
+++ greetery/src/App.js
@@ -1,12 +1,19 @@
-
+import { useState } from 'react';
+import { cardsData } from './cardsData'
+import Card from './component/Card'
 import './App.css';
 
 function App() {
+  const [cards] = useState(cardsData)
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>This is our app</h1>
       </header>
+      {cards.map((card, index) => (
+        <Card title={card.title} key={index} message={card.message}/>
+      ))}
     </div>
   );
 }
---
+++ greetery/src/cardsData.js
@@ -0,0 +1,22 @@
+export const cardsData = [
+    {
+        title: "Christmas Card",
+        message: "Have a Merry Christmas"
+        
+    },
+    {
+        title: "Easter Card",
+        message: "Have a Happy Easter"
+        
+    },
+    {
+        title: "Valentines Card",
+        message: "Have a Happy Valentines"
+        
+    },
+    {
+        title: "Happy Birthday",
+        message: "Have a Happy Birthday"
+        
+    },
+]
