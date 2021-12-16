d7276f960cc3d83cf229c679beb7e4333d2aef5e
janelle-bouchard <91432686+janelle-bouchard@users.noreply.github.com>
2021-12-16 17:11:22 -0500

    Added React routes and a file for the Card Form component with some notes about input types

---
+++ greetery/src/component/Forms.js
@@ -0,0 +1,14 @@
+// card.title = text
+// card.outer_message = text
+// card.outer_typefont = buttons or dropdown
+// card.outer_message_color = buttons or dropdown
+// card.outer_color = buttons or drowndown
+// card.author (hidden, but needed for database)
+// card.date_created (hidden, but needed for database)
+// card.image = image (look into unsplash, but can provide some basic urls initially for testing)
+// card.public (boolean, set to true)
+// card.tag = checkbox/list
+
+// export const CreateCard = () => {
+//     const
+// } 
