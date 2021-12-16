d5534f966a3e32d229fe08bbb553d4dc5a8a1072
Marsigli <marsigli1988@gmail.com>
2021-12-16 11:16:23 -0500

    added icons in navbar for the greetery sleigh, home button, and user profile button

--- greetery/src/component/Navbar.js
+++ greetery/src/component/Navbar.js
@@ -4,11 +4,16 @@
 const Navbar = () => {
     return (
         <nav>
-            <h2>GREETERY</h2>
+            <div className="greetery_navbar">
+                <h2>GREETERY</h2>
+                <h2 className='fas fa-sleigh'> </h2>
+            </div>
             <a href="/">MY GALLERY</a>
             <a href="/">FAV GALLERIES</a>
-            <a href="/">HOME</a>
-            <a href="/">PROFILE</a>
+            <div className="home_profile_navbar">
+                <a href="/" className="fas fa-home"> </a>
+                <a href="/"className="fas fa-user"> </a>
+            </div>
         </nav>
     )
 }
