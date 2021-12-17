d09f7c2ee457b78f46d9a3db3145e9d3aa03845c
Marsigli <marsigli1988@gmail.com>
2021-12-17 17:10:34 -0500

    added Create New Card element in navbar and created the path for it

--- greetery/src/component/Navbar.js
+++ greetery/src/component/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
                 <h2 className='fas fa-sleigh'> </h2>
             </div>
             <a href="/">MY GALLERY</a>
-            <a href="/">FAV GALLERIES</a>
+            <a href="/addcard">CREATE NEW CARD</a>
             <div className="home_profile_navbar">
                 <a href="/" className="fas fa-home"> </a>
                 <a href="/"className="fas fa-user"> </a>
