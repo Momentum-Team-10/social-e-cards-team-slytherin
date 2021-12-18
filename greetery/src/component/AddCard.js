65acbe81b0bc1da738487c18c14968c66629dc33
janelle-bouchard <91432686+janelle-bouchard@users.noreply.github.com>
2021-12-18 18:11:36 -0500

    Updated some styling on the add card page and added db.json to the repo to simulate our database for testing

--- greetery/src/component/AddCard.js
+++ greetery/src/component/AddCard.js
@@ -12,11 +12,8 @@
 import '../AddCard.css'
 import { useState } from 'react'
 
-// export const ColorChangeButton = (props) => {
-//   return (
-//     <button className={props.color} onClick={() => props.setColor(props.color)}>{props.color}</button>
-//   )
-// }
+
+const url = "http://localhost:3000/cards"
 
 export const AddCard = () => {
   const [title, setTitle] = useState('')
@@ -24,31 +21,74 @@ export const AddCard = () => {
   const [bgcolor, setBgcolor] = useState('')
   // const handleChange = (event) => {
   //   setMessage(event.target.value)
-
+  // Create a function that adds the notes to the database once they're created with a POST request
+  const createCard = (title, message, bgcolor) => {
+  fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+          // id: id.value,
+          title: title.value,
+          body: message.value,
+          color: bgcolor.value,
+          // created_at: moment().format()
+      })
+  })
+  .then(res => res.json())
+  // .then(cards => renderCardItem(cards))
+}
+  // Create a function that calls other functions (to add the note to the page and the database,) once the submit button is clicked
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    createCard(title, message, bgcolor)
+    // form.reset()
+  //   document.getElementById('title').value
+  //   document.getElementById('message').value
+  //   
+  //   createNote(noteTitle, noteText)
+  //   form.reset()
+  // })
+  }
   return (
-    <>
-      <div id="preview">
-        <h2>Card Preview</h2>
-        <h3>Title: {title}</h3>
-        <div id="output">  
-          <div className={bgcolor}>
-            <p>{message}</p>
+    <fragment id="addcard">
+        <div id="preview">
+          <h2>Card Preview</h2>
+          <h3>Title: {title}</h3>
+          <div id="output" className={bgcolor}>  
+              <p>{message}</p>
           </div>
         </div>
-      </div>
-      <form>
-        <input type="text" value={title} name="title" onChange={(e) => setTitle(e.target.value)} />
-        <input type="text" value={message} name="message" onChange={(e) => setMessage(e.target.value)} />
-        <div id="colors">
-            <button className="pink" value="pink" onClick={(e) => setBgcolor(e.target.value)}>pink</button>
-            <button className="blue" value="blue" onClick={(e) => setBgcolor(e.target.value)}>blue</button>
-            <button className="green" value="green" onClick={(e) => setBgcolor(e.target.value)}>green</button>
+        <div id="cardform">
+          <h2>Customize your card!</h2>
+          <form onSubmit={handleSubmit} id="add-card">
+            <div className="input-field" id="card-title-field">
+            <label htmlFor='title' id="title-label">Title:</label><br />
+            <input id="title" type="text-box" value={title} name="title" placeholder="Give your card a title!" onChange={(e) => setTitle(e.target.value)} />
+            </div>
+            <div className="input-field" id="card-message-field">
+            <label htmlFor='message'>Message:</label><br />
+            <input type="text" value={message} name="message" onChange={(e) => setMessage(e.target.value)} />
+            </div>
+            <div id="colors">
+              <label htmlFor='bgcolor'>Card color:</label><br />
+                <button className="pink" value="pink" onClick={(e) => setBgcolor(e.target.value)}>pink</button>
+                <button className="blue" value="blue" onClick={(e) => setBgcolor(e.target.value)}>blue</button>
+                <button className="green" value="green" onClick={(e) => setBgcolor(e.target.value)}>green</button>
+            </div>
+            <button type="submit">Done!</button>
+          </form>
         </div>
-      </form>
-    </>
+    </fragment>
   )
 } 
 
+
+
+// export const ColorChangeButton = (props) => {
+//   return (
+//     <button className={props.color} onClick={() => props.setColor(props.color)}>{props.color}</button>
+//   )
+// }
 
 // Radio buttons (and checkboxes) are different because they rely on a `checked` attribute
 // That is a boolean on the element
