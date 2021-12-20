eec95886c7e0244375f671500d539d7f779cda7d
Marsigli <marsigli1988@gmail.com>
2021-12-19 19:24:34 -0500

    added favorite button to card.js

--- greetery/src/cardsData.js
+++ greetery/src/cardsData.js
@@ -4,26 +4,31 @@ export const cardsData = [
         title: "Christmas Card",
         message: "Have a Merry Christmas",
         bgcolor: "blue",
+        favorited: true,
     },
     {
         id: 2,
         title: "Easter Card",
         message: "Have a Happy Easter",
         bgcolor: "green",
+        favorited: true,
     },
     {
         title: "Valentines Card",
         message: "Have a Happy Valentines",
         bgcolor: "pink",
+        favorited: true,
     },
     {
         title: "Happy Birthday",
-        message: "Have a Happy Birthday"
+        message: "Have a Happy Birthday",
+        favorited: true,
         
     },
     {
         title: "Happy Fourth of July",
-        message: "Have a great Fourth of July"
+        message: "Have a great Fourth of July",
+        favorited: true,
         
     },
 ]
